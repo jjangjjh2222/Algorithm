ce553dd7a5df3dadd5bd89b1bb8f05cfbbc7dd11
jjangjjh2222 <jjangjjh2222@naver.com>
2022-10-25 21:16:36 +0900

    [ADD] iteration triangle

--- recursive/recursive_1.py
+++ recursive/recursive_1.py
@@ -4,3 +4,16 @@ def printStar(n):
         printStar(n-1)
 
 printStar(5)
+
+
+def printStar2(n):
+    for i in range(1, n+1):
+        print("*" * i)
+
+printStar2(5)
+
+def printStar3(n):
+    for i in range(n, 0, -1):
+        print("*" * i)
+
+printStar3(5)
